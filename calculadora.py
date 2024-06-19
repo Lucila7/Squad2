85c3d0f84255e4a9202ea785ec0dfa86e9b116c3
Lucila7 <109292195+Lucila7@users.noreply.github.com>
2024-06-19 19:44:55 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,12 @@
+#Atividade de criação de uma calculadora utilizando match/case
+
+nome = input("Nome de usuário: ")
 conta = input("Defina se quer: + somar,  - subtrair, * multiplicar ou / dividir: ")
 valor1 = float(input("Número 1: "))
 valor2 = float(input("Número 2: "))
 
+#Calculando a operação escolhida
 match conta:
-    case '+':
-        print(f'{valor1} + {valor2}')
+    case '+' | 'somar' :
+        print(f'{nome}, a soma dos valores {valor1} e {valor2}:')
         print(valor1 + valor2)
