4392f473a7fb11a4ebf7f41466963a54af8f2f02
Murillo Barrios <muruca2006@gmail.com>
2024-06-19 19:35:06 -0300

    Adicionado os Inputs e a operação Soma

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+conta = input("Defina se quer: + somar,  - subtrair, * multiplicar ou / dividir: ")
+valor1 = float(input("Número 1: "))
+valor2 = float(input("Número 2: "))
+
+match conta:
+    case '+':
+        print(f'{valor1} + {valor2}')
+        print(valor1 + valor2)
